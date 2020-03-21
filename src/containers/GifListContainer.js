710d9f136c697fb4085ad54fc2c55aba1a0c2fd5
jessesbyers <jesse.s.byers@gmail.com>
2020-03-21 14:55:04 -0400

    Done.

--- src/containers/GifListContainer.js
+++ src/containers/GifListContainer.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 
-// import GifList from '/Users/jessebyers/Development/code/react-async-gif-search-lab-online-web-ft-110419/src/components/GifList'
+import GifList from '/Users/jessebyers/Development/code/react-async-gif-search-lab-online-web-ft-110419/src/components/GifList'
 import GifSearch from '/Users/jessebyers/Development/code/react-async-gif-search-lab-online-web-ft-110419/src/components/GifSearch'
 
 
@@ -27,23 +27,20 @@ class GifListContainer extends Component {
         })
     }
 
+    handleSubmit = () => {
+        console.log("handleSubmit function")
+    }
+
 
     render() {
         console.log("GifListContainer rendering")
         return (
         <div>
-            <GifSearch />
+            <GifSearch  onHandleSubmit={this.handleSubmit}/>
+            <GifList results={this.state}/>
         </div>
         )
     }
 }
-
-
-// responsible for fetching the data from the giphy API, 
-// storing the first 3 gifs from the response in its component state, 
-// and passing that data down to its child, the <GifList> component, as a prop.
-
-// It will also render a <GifSearch /> component that renders the form. 
-// <GifListContainer /> should pass down a submit handler function to <GifSearch /> as a prop.
 
 export default GifListContainer;
