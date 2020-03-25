b3c271ca1dd6a847d86be64e513f21ffb68b4b18
cejimenez0123 <christianjimenez0123@gmail.com>
2020-03-25 14:51:50 -0400

    Done.

---
+++ src/components/GifList.js
@@ -0,0 +1,24 @@
+import React, {Component} from 'react'
+
+export default class GifList extends Component{
+
+    gifList = () => {
+
+
+    return this.props.gifs.map((gif,index) =>{  
+        
+         return( 
+        
+             <li key={index}><img key={gif.id} src={gif.images.original.url} alt="gif" /></li>
+            
+         )
+        }
+        ).slice(0,3)
+    }
+    
+   render(){
+  
+   return (<ul>{this.gifList()}</ul>
+   )
+   }
+}
